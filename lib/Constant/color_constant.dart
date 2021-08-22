bbb146d8b0ad86d0b6f769e534731cf9313aed02
Yoga Prasetiyo <yoga.xv@gmail.com>
2021-08-22 22:14:31 +0700

    Create color_constant.dart

---
+++ lib/Constant/color_constant.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color primaryText = Color.fromRGBO(72, 77, 87, 1);
+  static const Color primaryBackground = Color.fromRGBO(196, 243, 233, 1);
+}
