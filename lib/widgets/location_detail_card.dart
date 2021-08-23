3a344d768f4398f9a1909da23f2dabfa38288754
Yoga Prasetiyo <yoga.xv@gmail.com>
2021-08-23 22:02:30 +0700

    Create location_detail_card.dart

---
+++ lib/widgets/location_detail_card.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/Constant/color_constant.dart';
+
+class LocationDetailCard extends StatelessWidget {
+  const LocationDetailCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: ColorConstant.primaryText,
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bogor",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      "Wed, 16 July 2021 3:45 pm",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontSize: 10.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 10.0,
+                  height: 10.0,
+                  decoration: new BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 35),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "28\u2103",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 3),
+                    Text(
+                      "Sunny day",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "Tomorrow 23\u2103 / 28\u2103",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.5),
+                    fontSize: 10.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
